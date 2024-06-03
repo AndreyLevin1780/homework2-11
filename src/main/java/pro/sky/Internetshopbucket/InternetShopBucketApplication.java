34066b69da30dd1ccf12871e2d582d01371f734a
AndreyLevin1780 <andreylevin1980@mail.ru>
2024-06-03 23:27:37 +0300

    initial commit

---
+++ src/main/java/pro/sky/Internetshopbucket/InternetShopBucketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Internetshopbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopBucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Internetshopbucket/InternetShopBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Internetshopbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
